ef490d6fc093994f64e12c3557fcb7fc0321293c
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2025-04-13 13:14:58 -0400

    automatic update

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -4,8 +4,6 @@ $(function() {
 
     for (let i=0; i< scoreCardCollection.length; i++){
 
-        //TODO do something to test if my scoreCardCollection works!
-
         s = scoreCardCollection[i].name
 
         $("#vote-list-outer").append("<p>" + s + "</p>");

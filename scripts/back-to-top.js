4f3301f8e32f6335a2f4937cd7886c79dd92aec8
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2025-10-27 10:09:06 -0400

    automatic update

--- scripts/back-to-top.js
+++ scripts/back-to-top.js
@@ -1,6 +1,6 @@
 //Make the button:
 var mybutton = document.createElement("button");
-mybutton.innerHTML = '<div id="chevron" style="align-items:center;align-content:center;justify-content:center;"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="#ABB2B9" class="bi bi-chevron-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/></svg></div>'
+mybutton.innerHTML = '<div id="chevron" style="align-items:center;align-content:center;justify-content:center;"><svg xmlns="https://www.w3.org/2000/svg" width="2em" height="2em" fill="#ABB2B9" class="bi bi-chevron-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/></svg></div>'
 document.body.appendChild(mybutton);
 mybutton.setAttribute('id', 'back-to-top');
 mybutton.setAttribute('onclick', 'topFunction()');

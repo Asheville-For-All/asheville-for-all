9a0b0433764ee58533600164679b9fad3392d48d
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2026-02-06 08:14:45 -0500

    automatic update

--- scripts/questionnaire.js
+++ scripts/questionnaire.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 
     checkParams();
+    buildFilterButton();
+    populateModal();
 };
 
 function checkParams(){
@@ -38,10 +40,56 @@ function showDisclaimer(name){
 
     var url = window.location.href.split('?')[0];
 
-    var questionnaireInfo = document.getElementById("questionnaire-info");
+    var filterInfo = document.getElementById("filter-info");
 
-    var disclaimer = document.createElement('p');
-    disclaimer.innerHTML = "Now showing responses for " + name + ". <a href='" + url + "'>Click here to reset the page.</a>";
+    filterInfo.innerHTML = "<p>Now showing responses for " + name + ". <a href='" + url + "'>Click here to reset the page.</a></p>"
+}
 
-    questionnaireInfo.append(disclaimer);
+function buildFilterButton(){
+    var mybutton = document.createElement("button");
+    mybutton.innerHTML = '<div id="filter-icon" style="align-items:center;align-content:center;justify-content:center;"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="#ABB2B9" class="bi bi-filter" viewBox="0 0 16 16"><path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/></svg></div>'
+    document.body.appendChild(mybutton);
+    mybutton.setAttribute('id', 'filter-button');
+    mybutton.setAttribute('title', 'filter');
+    mybutton.setAttribute('display', 'flex');
+    mybutton.setAttribute('data-bs-toggle', 'modal');
+    mybutton.setAttribute('data-bs-target', '#filter-modal');
+}
+
+function getNames(){
+    var s = new Set()
+    const responseDivs = document.querySelectorAll("div .response");
+
+    responseDivs.forEach((d) => {
+        s.add(d.dataset.name)
+    });
+    return s
+}
+
+function setFilter(){
+    console.log("button clicked.");
+    var url = window.location.href.split('?')[0];
+    const v = document.getElementById('filter-select').value;
+    if (v=="all"){
+        window.location.href = url;
+    }else{
+        window.location.href = url + "?name=" + encodeURI(v);
+    }
+}
+
+function populateModal(){
+    b = document.getElementById("filter-modal-body");
+    names = getNames();
+    s = document.getElementById("filter-select");
+    for (const item of names) {
+      o = document.createElement('option');
+      o.innerHTML = item;
+      o.setAttribute('val', item);
+      s.append(o);
+
+    //Activate filter-go-button
+    var btn = document.getElementById("filter-go-button");
+    btn.setAttribute('onclick', 'setFilter()');
+
+    }
 }

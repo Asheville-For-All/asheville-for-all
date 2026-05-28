277b90a8e6c3b29556a7d4f673ac124805946a48
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2026-05-28 11:11:43 -0400

    automatic update

--- scripts/gauge-modal.js
+++ scripts/gauge-modal.js
@@ -9,12 +9,14 @@ let content = `
 
           <div style="float:left; margin-right:1rem;">
           <div style="mask: url(img/tracker-imgs/file-lines-solid-full.svg);display:inline-block;" class="gauge-low"></div>
+          <div style="mask: url(img/tracker-imgs/file-lines-solid-full.svg);display:inline-block;" class="gauge-medium-low"></div>
           <div class="gauge-medium" style="mask: url(img/tracker-imgs/file-lines-solid-full.svg);display:inline-block;"></div>
+          <div class="gauge-medium-high" style="mask: url(img/tracker-imgs/file-lines-solid-full.svg);display:inline-block;"></div>
           <div class="gauge-high" style="mask: url(img/tracker-imgs/file-lines-solid-full.svg);display:inline-block;"></div>
           </div>
 
           <p>The paper icon illustrates Asheville For All's position on <b>the proposed agenda item</b>, at the time of the hearing.</p>
-          <p> The color reflects whether the agenda item was generally pro-housing, anti-housing, or something that Asheville For All was unable to determine by the time of the hearing.</p>
+          <p> The color reflects whether the agenda item was generally pro-housing or anti-housing. An intermediate color suggests that there may have been potentially complicating or confusing circumstances involved.</p>
           
       </div>
       <hr/>
@@ -22,13 +24,13 @@ let content = `
       <div class="clearfix">
 
           <div style="float:left; margin-right:1rem;">
-          <div style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;" class="gauge-low"></div>
-          <div class="gauge-medium" style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;"></div>
+          <div style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;" class="gauge-low"></div><div style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;" class="gauge-medium-low"></div>
+          <div class="gauge-medium" style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;"></div><div style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;" class="gauge-medium-high"></div>
           <div class="gauge-high" style="mask: url(img/tracker-imgs/gavel-solid-full.svg);display:inline-block;"></div>
           </div>
 
           <p>The gavel icon illustrates Asheville For All's assessment of <b>the final decision, or <i>motion</i></b> associated with the agenda item.</p>
-          <p> The color reflects whether the outcome was pro-housing, anti-housing, or something more complicated and/or ambiguous.</p>
+          <p> The color reflects whether the outcome was pro-housing or anti-housing. An intermediate color suggests an ambiguous outcome, or that there were complicating circumstances involved.</p>
           
       </div>
 

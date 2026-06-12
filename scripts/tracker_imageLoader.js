fbbfa67375f1888f6bcb673171d8464dc5d5074b
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2026-06-12 15:16:37 -0400

    automatic update

--- scripts/tracker_imageLoader.js
+++ scripts/tracker_imageLoader.js
@@ -22,7 +22,12 @@ export class ImageLoader {
             "/img/tracker-imgs/asterisk.svg",
             "/img/tracker-imgs/file-lines-solid-full.svg",
             "/img/tracker-imgs/gavel-solid-full.svg",
-            "/img/tracker-imgs/trophy-fill.svg"
+            "/img/tracker-imgs/trophy-fill.svg",
+            "/img/tracker-imgs/face-0.svg",
+            "/img/tracker-imgs/face-1.svg",
+            "/img/tracker-imgs/face-2.svg",
+            "/img/tracker-imgs/face-3.svg",
+            "/img/tracker-imgs/face-4.svg"
         ];
 
         var self = this;

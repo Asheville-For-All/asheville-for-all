eb60db348627ea33bb5fe1b75ad1f2dc079e6ebe
asheville-for-all-robot <asheville-for-all-robot@users.noreply.github.com>
2026-06-20 08:48:43 -0400

    automatic update

--- scripts/tracker-close-me.js
+++ scripts/tracker-close-me.js
@@ -6,13 +6,9 @@ function closeMe(triggeringElem){
     dialg = $(triggeringElem).closest('dialog');
   }
   if (dialg.get(0).requestClose){
-    console.log(dialg.attr('id'));
-    console.log("request close function is present.");
     dialg.get(0).requestClose();
   }
   else{
-    console.log(dialg.attr('id'));
-    console.log("no request close function found.");
     dialg.get(0).close();
   }
 }
